d717f03947e52ffaeb431f1b6371e4b1f66a072c
laroccrj <laroccrj@gmail.com>
2018-08-06 16:23:53 -0400

    Added docs to FrameCleaner

--- src/Domain/Frame/FrameCleaner.js
+++ src/Domain/Frame/FrameCleaner.js
@@ -1,4 +1,19 @@
 export default class FrameCleaner {
+  /**
+   * A frame is a step a spell that is saved as a list of nodes where the spell is present
+   * ex: [{1,2},{2,2}]
+   *
+   * Each node of a frame must be present in the previous frame, or adjacent to a node in the previous frame
+   * Diagonal counts as adjecent
+   */
+
+  /**
+   * Checks if the nodes are possible for the next frames based off previous frame
+   * Removes impossible nodes
+   * @param previousFrame
+   * @param frames
+   * @returns {Array}
+   */
 	static cleanFrames(previousFrame, frames) {
 		if(!frames || frames.length == 0) return;
 
@@ -18,6 +33,12 @@ export default class FrameCleaner {
 		return newFrames;
 	}
 
+  /**
+   * Removes any nodes that are not possible based on the previous frame
+   * @param previousFrame
+   * @param frame
+   * @returns {*}
+   */
 	static cleanFrame(previousFrame, frame) {
 		var i = frame.length;
 
@@ -27,6 +48,12 @@ export default class FrameCleaner {
 		return frame;
 	}
 
+  /**
+   * Checks if the node is adjacent or diagonal to any nodes from the previous frame
+   * @param previousFrame
+   * @param node
+   * @returns {boolean}
+   */
 	static nodePossible(previousFrame, node) {
 		var i = previousFrame.length;
 
@@ -36,6 +63,12 @@ export default class FrameCleaner {
 		return false;
 	}
 
+  /**
+   * Checks if a node is adjacent or diagonal to another node
+   * @param node1
+   * @param node2
+   * @returns {boolean}
+   */
 	static touching(node1, node2) {
 		if(Math.abs(node1.x - node2.x) <= 1 && Math.abs(node1.y - node2.y) <= 1) {
 			return true;
@@ -44,6 +77,12 @@ export default class FrameCleaner {
 		return false;
 	}
 
+  /**
+   * Checks if two frames have the same nodes set
+   * @param frame1
+   * @param frame2
+   * @returns {boolean}
+   */
 	static compareFrames(frame1, frame2) {
 		var equal = true;
 
