56e276ea5321b897658b0cc9dd91a9a1f80ed39e
Jeff Andolora <jeeeeefff@gmail.com>
2018-08-06 19:55:58 -0400

    Add a copy to clipboard button

--- src/components/SpellBuilder/SpellBuilder.vue
+++ src/components/SpellBuilder/SpellBuilder.vue
@@ -39,16 +39,20 @@
     </div>
     <div>
       <textarea v-model="spellJSON" class='spellLoader'></textarea>
+      <button type="button" v-clipboard:copy="spellJSON">Copy!</button>
     </div>
 	</div>
 </template>
 
 <script>
 import Vue from 'vue'
+import VueClipboard from 'vue-clipboard2'
 import Grid from '@/Domain/Grid/Grid'
 import FrameCleaner from '@/Domain/Frame/FrameCleaner'
 import PlayerNode from '@/components/SpellBuilder/Nodes/PlayerNode'
 import SpellNode from '@/components/SpellBuilder/Nodes/SpellNode'
+
+Vue.use(VueClipboard)
 
 export default {
   name: 'SpellBuilder',
