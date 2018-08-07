50514031ca6817a70a41a00563cfbb1af49bfbe1
laroccrj <laroccrj@gmail.com>
2018-08-06 23:02:33 -0400

    Made game component pretty

---
+++ src/components/Game/Game.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<h1>Game Time</h1>
+	</div>
+</template>
+
+<script>
+export default {
+
+  name: 'Game',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
