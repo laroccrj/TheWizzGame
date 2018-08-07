a5144bbdabcdfce16d06c30b95fd247a6fc23d91
laroccrj <laroccrj@gmail.com>
2018-08-06 23:36:06 -0400

    Extracted grid component

---
+++ src/components/Grid.vue
@@ -0,0 +1,44 @@
+<template>
+	<table>
+		<tr v-for="row in displayGrid">
+			<td v-for="node in row.columns">
+				<component :is="node.val.component"
+									 :row="node.row"
+									 :column="node.column"
+									 :value="node.value"
+				></component>
+			</td>
+		</tr>
+	</table>
+</template>
+
+<script>
+import Grid from '@/Domain/Grid/Grid'
+import SpellNode from '@/components/SpellBuilder/Nodes/SpellNode'
+
+export default {
+
+  name: 'grid',
+
+  data () {
+    return {
+    	displayGrid: [],
+    }
+  },
+  props: {
+  	rows: Number,
+  	columns: Number,
+  	defaultComponent: String
+  },
+  created: function () {
+    this.grid = new Grid(this.columns, this.rows, {component:this.defaultComponent});
+    this.displayGrid = this.grid.getFormattedGrid();
+  },
+  components: {
+  	SpellNode
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- src/components/HomePageComponent.vue
+++ src/components/HomePageComponent.vue
@@ -3,14 +3,27 @@
 		<div>
 			<a href="/#/SpellBuilder/"> <button>Spell Creator</button></a>
 		</div>
+		<div>
+			<grid :rows=11
+						:columns=11
+						defaultComponent="SpellNode">
+				
+			</grid>
+		</div>
 	</div>
 </template>
 
 <script>
+import Vue from 'vue'
+import grid from './Grid.vue'
+
 export default {
-  name: 'HomePageComponent',
-  data() {
-    return {}
-  }
+	name: 'HomePageComponent',
+	data() {
+		return {}
+	},
+	components: {
+		grid
+	}
 }
 </script>
