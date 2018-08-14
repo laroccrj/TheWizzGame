a4a19b9cbdff45efb715c85d53fdfd8d1266de42
Jeff Andolora <jeeeeefff@gmail.com>
2018-08-14 16:21:18 -0400

    Responsive spell builder grid and common header

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,35 @@
 <template>
   <div id="app">
-    <h1>Wizzards</h1>
+    <Header/>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header
+  }
 }
 </script>
 
 <style>
+* {
+  border: 0;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 button {
@@ -26,12 +37,16 @@ button {
   outline: 0;
   padding: 10px;
   border-radius: 10px;
-  background: #ff9f9f;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  transition: all 0.4s cubic-bezier(0.25, 0.8, 0.25, 1);
+  background: white;
+  border: 1px solid #ff9f9f;
+  transition: all 0.8s cubic-bezier(0.25, 0.8, 0.25, 1);
 }
 
 button:hover {
   box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+}
+
+button:active {
+  background: #ff9f9f;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="header">
+    <h1 class="header-title">Wizzards</h1>
+    <a class="header-link" href="/#/SpellBuilder/">Spell Creator</a>
+    <a class="header-link" href="/#/game/">Game</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+.header {
+  background-color: #f3e8e8;
+  text-align: left;
+  width: 100%;
+  max-width: 600px;
+  margin: auto;
+  margin-top: 10px;
+  margin-bottom: 25px;
+  color: #3f000b;
+  height: 60px;
+  box-shadow: 0 1px 8px rgba(0, 0, 0, 0.15);
+  border-radius: 10px;
+}
+.header-title,
+.header-link {
+  display: inline-block;
+  height: 60px;
+  line-height: 60px;
+  vertical-align: middle;
+}
+.header-link {
+  width: 160px;
+  text-align: center;
+  text-decoration: none;
+  color: inherit;
+  font-weight: bold;
+  font-size: 110%;
+  float: right;
+  border-radius: 10px;
+  padding: 0 10px;
+  margin: 0 10px;
+}
+.header-link:hover {
+  background-color: #f3caca;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.25);
+}
+.header-title {
+  text-shadow: -2px 2px white;
+  padding: 0 20px;
+}
+
+@media screen and (max-width: 540px) {
+  .header-link {
+    padding: 0;
+    margin: 0;
+  }
+}
+
+@media screen and (max-width: 500px) {
+  .header-link {
+    font-size: 100%;
+    width: 115px;
+  }
+}
+
+@media screen and (max-width: 410px) {
+  .header-title {
+    padding: 0 0 0 10px;
+    font-size: 160%;
+    float: left;
+  }
+}
+
+@media screen and (max-width: 360px) {
+  .header-link {
+    font-size: 90%;
+    width: 95px;
+  }
+}
+</style>
